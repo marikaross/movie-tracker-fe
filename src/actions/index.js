87bf24eb8b4f17ba2c984ea345e1c9adabd61843
dsdunn <dsdunnmusic@gmail.com>
2018-07-11 16:48:36 -0600

    Make actionCreators and reducers

---
+++ src/actions/index.js
@@ -0,0 +1,13 @@
+export const addMovies = (movies) => ({
+  type: "ADD_MOVIES",
+  movies
+})
+
+export const toggleFav = (id) => {
+  type: "TOGGLE_FAV",
+  id
+}
+
+export const toggleFilter = () => {
+  type: "TOGGLE_FILTER"
+}
