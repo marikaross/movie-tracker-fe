160e043a955c59bbb722470af1b14fa99f0e2b72
Joel O'Connor <joconno07@gmail.com>
2018-07-11 11:10:19 -0600

    Add initial file structure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import apiKey from './apiKey';
 import './App.css';
 
 class App extends Component {
+
+  componentDidMount() => {
+    const url = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&primary_release_year=2018`;
+
+  }
+
   render() {
     return (
       <div className="App">
