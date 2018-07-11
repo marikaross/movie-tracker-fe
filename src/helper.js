237f984050ab8cf28b2ed0aaf17cca71e00f38f2
Joel O'Connor <joconno07@gmail.com>
2018-07-11 11:57:01 -0600

    Update linter and globbing pattern

--- src/helper.js
+++ src/helper.js
@@ -1,6 +1,5 @@
 export const moviesCleaner = (movies) => {
-
-  const cleanMovies = movies.results.map( movie => {
+  return movies.results.map( movie => {
     return {
       id: movie.id,
       title: movie.title,
@@ -8,9 +7,9 @@ export const moviesCleaner = (movies) => {
       releaseDate: movie.release_date,
       voteAverage: movie.vote_average,
       overview: movie.overview
-    }
+    };
   });
 
-  
-  
-}
+
+
+};
