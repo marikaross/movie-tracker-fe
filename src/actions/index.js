f29def2d93c0603ba2813f4c4ee63081838195d5
dsdunn <dsdunnmusic@gmail.com>
2018-07-13 11:38:34 -0600

    Make userReducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -12,3 +12,20 @@ export const toggleFilter = () => ({
   type: "TOGGLE_FILTER"
 
 });
+
+export const loginUser = (user) => ({
+  type: "LOGIN_USER",
+  user
+})
+
+export const signUpUser = (name, email, password) => ({
+  type: "SIGN_UP_USER",
+  name,
+  email,
+  password
+})
+
+export const logOut = () => ({
+  type: "LOG-OUT"
+
+})
