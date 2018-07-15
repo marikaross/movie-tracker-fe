f0f9068beaeac55a2084cf0eeed0ed954b382cad
Marika Ross <marika.ross@gmail.com>
2018-07-15 13:57:45 -0600

    Write tests for logIn

--- src/api-calls.test.js
+++ src/api-calls.test.js
@@ -1,17 +1,71 @@
 import React from 'react';
-import { getMovies } from './api-calls.js';
+import {getMovies, logIn, signUp} from './api-calls.js';
 import  { apiKey } from './apiKey.js';
+import { 
+  mockMovieData, 
+  mockUserData } from './mockMovieData.js';
 
 
 describe ('getMovies', () => {
+
+  beforeEach(() => {
+    window.fetch = jest.fn().mockImplementation(() => Promise.resolve({ status: 200, json: () => Promise.resolve(mockMovieData) }))
+  })
+
   it('should call fetch with the correct parameters', async () => {
     const url = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&primary_release_year=2018`
-    window.fetch = jest.fn().mockImplementation(() => Promise.resolve({ json: () => Promise.resolve() }))
     await getMovies();
     expect(window.fetch).toHaveBeenCalledWith(url);
   })
 
-  it('should return the correct data', () => {
-    
+  it('should return the correct data', async () => {
+    const expected = mockMovieData;
+    const result = await getMovies()
+    expect(result).toEqual(expected)
+  })
+
+  it('should return an error if the fetch fails', async () => {
+    window.fetch = jest.fn().mockImplementation(() => Promise.reject(Error('Fetch failed')))
+    const expected = 'Fetch failed'
+    const result = await getMovies()
+    expect(result).toEqual(expected);
   })
 })
+
+describe ('logIn', () => {
+  let url 
+  beforeEach(() => {
+    window.fetch = jest.fn().mockImplementation(() => Promise.resolve({status: 200, json: () => Promise.resolve(mockUserData) }))
+
+    url = 'http://localhost:3000/api/users/'
+  })
+
+  it('should be called with the correct url', async () => {
+    const mockOptionsObject = {
+      method: 'POST',
+      body: JSON.stringify({}),
+      headers:{
+        'Content-Type': 'application/json'
+      }}
+
+    await logIn(url, mockOptionsObject);
+    expect(window.fetch).toHaveBeenCalledWith(url, mockOptionsObject);
+  })
+
+  it('should return the correct data', async() => {
+    const url = 'http://localhost:3000/api/users/'
+    const expected = mockUserData.data
+    const result = await logIn(url)
+    expect(result).toEqual(expected)
+  })
+
+  it('should return an error if the fetch fails', async() => {
+    window.fetch = jest.fn().mockImplementation(() => Promise.reject(Error(false)))
+    const expected = false
+    const result = await logIn(url)
+    expect(result).toEqual(expected)
+  })
+
+})
+
+describe ('signUp', )
