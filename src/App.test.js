381371b0aa28988c5ac7e5d9b9b9e820c50b8cb9
Marika Ross <marika.ross@gmail.com>
2018-07-16 08:03:22 -0600

    Add setupTests file and write App snapshot

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import * as apiCalls from './api-calls';
+import { moviesCleaner } from './helper.js';
+import { shallow } from 'enzyme';
 
-it('renders without crashing', () => {
+  
+describe('App', () => {
+  it.skip('matches the snapshot', () => {
+    const mockAddRecentMovies = jest.fn()
+    const wrapper = shallow(<App addRecentMovies={mockAddRecentMovies} />)
+    expect(wrapper).toEqualSnapshot()
+  })
+  describe('componentDidMount', () => {
+    it.skip('calls props.addRecentMovies with the correct parameters', async () => {
+      apiCalls.getMovies = jest.fn().mockImplementation(() => Promise.resolve({results: ['movies']}))
+      moviesCleaner = jest.fn().mockImplementation(() => ['movies'])
+      const mockAddRecentMovies = jest.fn()
+      const wrapper = await shallow(<App addRecentMovies={mockAddRecentMovies} />);
 
-});
+      expect(wrapper.instance().props.addRecentMovies).toHaveBeenCalledWith(['movies'])
+    })
+  })
+
+
+})
