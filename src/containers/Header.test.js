e4c9fa0999b63063a2ade61bb71974660a2b6082
Marika Ross <marika.ross@gmail.com>
2018-07-18 05:19:09 -0600

    Write initial snapshot test for Header

--- src/containers/Header.test.js
+++ src/containers/Header.test.js
@@ -1,8 +1,29 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import * as action from '../actions';
-import { logInUser }
+import { Header, mapStateToProps, mapDispatchToProps } from './Header.js'
 
 
 
-describe()
+describe('header', () => {
+  let wrapper
+  let mockToggleFilter
+  let mockLogOutUser
+
+  beforeEach(() => {
+    togglefilter = jest.fn()
+    logOutUser = jest.fn()
+    wrapper = mount(<Header 
+      toggleFilter={mockToggleFilter}
+      logOutUser={mockLogOutUser}/>)
+
+  })
+  it('should match the snapshot', () => {
+    let wrapper = shallow(<Header />)
+    expect(wrapper).toMatchSnapshot()
+  })
+
+  it('calls toggleFilter on a click of the favorites button', () => {
+    wrapper.find('.')
+  })
+})
