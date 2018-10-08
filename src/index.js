236134aa6abb9f6a2aa71de8decbbad919916649
Marika Ross <marika.ross@gmail.com>
2018-10-08 13:31:26 -0600

    Apply middleware

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import { rootReducer } from './reducers';
 import { BrowserRouter } from 'react-router-dom';
